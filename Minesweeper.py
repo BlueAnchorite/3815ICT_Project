477d8280a70685880cc7e9b4b6ba81a71f40199a
BlueAnchorite <the_jman1993@hotmail.com>
2018-09-14 17:21:53 +1000

    Refactored storage for tiles and added method to find surrounding neighbours of a selected tile

--- Minesweeper.py
+++ Minesweeper.py
@@ -22,30 +22,53 @@ def click(event):
         print(clicked[0])
         clicked_tile = find_tile(clicked[0])
         print(clicked_tile.x, clicked_tile.y)
+        find_neighbours(clicked_tile)
         canvas.update_idletasks()
         canvas.after(200)
         canvas.itemconfig(CURRENT, fill="red")
 
 
 def find_neighbours(clicked_tile):
-    pass
+    neighbours = []
+    if clicked_tile.y == 0:
+        y_start = 0
+    elif clicked_tile.y == tiles_y - 1:
+        y_end = tiles_y - 1
+    else:
+        y_start = clicked_tile.y - 1
+        y_end = clicked_tile.y + 2
+
+    if clicked_tile.x == 0:
+        x_start = 0
+    elif clicked_tile.x == tiles_x - 1:
+        x_end = tiles_x - 1
+    else:
+        x_start = clicked_tile.x - 1
+        x_end = clicked_tile.x + 2
+
+    for y in range(y_start, y_end):
+        for x in range(x_start, x_end):
+            neighbours.append(tiles[x][y])
+    for i in neighbours:
+        print(i.x, i.y)
 
 
 def find_tile(find_id):
-    for item in tiles:
-        if item.canvas_id == find_id:
-            return item
+    for column in tiles:
+        for item in column:
+            if item.canvas_id == find_id:
+                return item
 
 
 items = []
-tiles = []
+tiles = [[] for i in range(tiles_x)]
 for i in range(tiles_y):
     y = i * side_length
     for f in range(tiles_x):
         x = side_length * f
         canvas_id = canvas.create_rectangle(x, y, x + side_length, y + side_length, fill="red")
         new_tile = Tile(f, i, canvas_id)
-        tiles.append(new_tile)
+        tiles[i].append(new_tile)
 
 canvas.bind("<Button-1>", click)
 items = canvas.find_all()
